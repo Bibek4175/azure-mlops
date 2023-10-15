ee107fd0dd916bca246ac20211e0f0b96df8dada
Bibek4175 <chandbibak09@gmail.com>
2023-10-15 09:55:24 +0545

    linting code

--- src/model/train.py
+++ src/model/train.py
@@ -1,42 +1,42 @@
-# Import libraries 
-import argparse 
-import glob 
-import os 
-import pandas as pd 
- 
-from sklearn.linear_model import LogisticRegression 
-from sklearn.model_selection import train_test_split 
-from mlflow.sklearn import autolog 
- 
-# define functions 
-def main(args): 
- 
+# Import libraries
+import argparse
+import glob
+import os
+import pandas as pd
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from mlflow.sklearn import autolog
+
+# define functions
+def main(args):
+
     # enable autologging
-    autolog() 
- 
-    # read data 
-    df = get_csvs_df(args.training_data) 
- 
-    # split data 
-    X_train, X_test, y_train, y_test = split_data(df) 
- 
-    # train model 
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test) 
- 
-def get_csvs_df(path): 
-    if not os.path.exists(path): 
-        raise RuntimeError(f"Cannot use non-existent path provided: {path}") 
-    csv_files = glob.glob(f"{path}/*.csv") 
-    if not csv_files: 
-        raise RuntimeError(f"No CSV files found in provided data path: {path}") 
-    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False) 
- 
+    autolog()
+
+    # read data
+    df = get_csvs_df(args.training_data)
+
+    # split data
+    X_train, X_test, y_train, y_test = split_data(df)
+
+    # train model
+    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+
+def get_csvs_df(path):
+    if not os.path.exists(path):
+        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
+    csv_files = glob.glob(f"{path}/*.csv")
+    if not csv_files:
+        raise RuntimeError(f"No CSV files found in provided data path: {path}"
+    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
+
 # our new function
-def split_data(df, test_size=0.2): 
-    X = df.drop("Diabetic", axis=1) 
-    y = df["Diabetic"] 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size) 
-    return X_train, X_test, y_train, y_test 
+def split_data(df, test_size=0.2):
+    X = df.drop("Diabetic", axis=1)
+    y = df["Diabetic"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    return X_train, X_test, y_train, y_test
  
 def train_model(reg_rate, X_train, X_test, y_train, y_test): 
     # train model 
