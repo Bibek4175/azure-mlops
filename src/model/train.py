30f271ece2af8a628d2990189460ad551648c9f5
Bibek4175 <chandbibak09@gmail.com>
2023-10-15 10:02:12 +0545

    linting code

--- src/model/train.py
+++ src/model/train.py
@@ -28,7 +28,7 @@ def get_csvs_df(path):
         raise RuntimeError(f"Cannot use non-existent path provided: {path}")
     csv_files = glob.glob(f"{path}/*.csv")
     if not csv_files:
-        raise RuntimeError(f"No CSV files found in provided data path: {path}"
+        raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 # our new function
@@ -49,6 +49,21 @@ def parse_args():
     # add arguments 
     parser.add_argument("--training_data", dest="training_data", type=str) 
     parser.add_argument("--reg_rate", dest="reg_rate", type=float, default=0.01) 
+ 
+    # parse args 
+    args = parser.parse_args() 
+ 
+    # return args 
+    return args 
+LogisticRegression(C=1 / reg_rate, solver="liblinear").fit(X_train, y_train>
+ 
+def parse_args(): 
+    # setup arg parser 
+    parser = argparse.ArgumentParser() 
+ 
+    # add arguments 
+    parser.add_argument("--training_data", dest="training_data", type=str) 
+    parser.add_argument("--reg_rate", dest="reg_rate", type=float, default=0.01>
  
     # parse args 
     args = parser.parse_args() 
